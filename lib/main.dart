75de5cc69fee47e35ceb8eb8d8f7b342ef9568cb
JorgeCruz134 <cruzordazjorge1804@gmail.com>
2026-04-27 08:40:50 -0600

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart' show kIsWeb; // ✅ Detecta si es Web
 import 'firebase_options.dart';
 import 'theme/app_theme.dart';
 import 'screens/login_screen.dart';
@@ -8,10 +9,25 @@ void main() async {
   // 1. Asegura que Flutter esté listo
   WidgetsFlutterBinding.ensureInitialized();
 
-  // 2. Conecta la App con Firebase
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  // 2. Conecta la App con Firebase usando lógica dual (Web/Móvil)
+  if (kIsWeb) {
+    // ✅ Configuración para que funcione en Vercel y iPhone (Safari)
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+        apiKey: "AIzaSyAgZ054dZcEnaBBQfMqtvROeh88SGbALHk",
+        authDomain: "gestion-ahimet.firebaseapp.com",
+        projectId: "gestion-ahimet",
+        storageBucket: "gestion-ahimet.firebasestorage.app",
+        messagingSenderId: "277415438277",
+        appId: "1:277415438277:web:8a734cf1068bb411978dac",
+      ),
+    );
+  } else {
+    // Esto se queda igual para tu celular OPPO
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+  }
 
   runApp(const MyApp());
 }
@@ -27,7 +43,7 @@ class MyApp extends StatelessWidget {
       // --- MAGIA AQUÍ ---
       theme: AppTheme.lightTheme,
       // ------------------
-      home: LoginScreen(),
+      home: const LoginScreen(),
     );
   }
 }
